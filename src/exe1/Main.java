33f74fa0d04102cc4a125e999dc336e5fd9a56d0
flakennn <qwer2323vex@yandex.ru>
2022-11-20 15:08:17 +0300

    123

--- src/exe1/Main.java
+++ src/exe1/Main.java
@@ -1,4 +1,4 @@
-package exc;
+package exe1;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,10 +9,10 @@ public class Main {
             System.err.print(" 1");
         }
         catch (Exception e)
-        { // catch по Exception ПЕРЕХВАТЫВАЕТ RuntimeException
+        {
             System.err.print(" 2");
         }
         System.err.println(" 3");
-    }// end main
+    }
 }
 
